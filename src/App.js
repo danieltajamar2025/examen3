0d8e3e4fe230700a4baa59342e8ff49a89ac9c4d
Daniel <daniel.gomezjimenez@tajamar365.com>
2025-07-28 18:16:01 +0200

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Mi App desplegada en AWS S3</h1>
+      <p>Aplicación React simple lista para despliegue</p>
+    </div>
+  );
+}
+
+export default App;
